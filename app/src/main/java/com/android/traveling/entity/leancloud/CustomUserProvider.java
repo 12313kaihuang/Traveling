4bc978af15a9f24a538bbd9556182ab6a614f43f
12313kaihuang <2356048622@qq.com>
2019-03-15 22:02:03 +0800

    继承部分LeanCloud

--- app/src/main/java/com/android/traveling/entity/leancloud/CustomUserProvider.java
+++ app/src/main/java/com/android/traveling/entity/leancloud/CustomUserProvider.java
@@ -1,5 +1,8 @@
 package com.android.traveling.entity.leancloud;
 
+import com.android.traveling.util.LogUtil;
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,45 +16,46 @@ import cn.leancloud.chatkit.LCChatProfilesCallBack;
  */
 public class CustomUserProvider implements LCChatProfileProvider {
 
-  private static CustomUserProvider customUserProvider;
+    private static CustomUserProvider customUserProvider;
 
-  public synchronized static CustomUserProvider getInstance() {
-    if (null == customUserProvider) {
-      customUserProvider = new CustomUserProvider();
-    }
-    return customUserProvider;
-  }
-
-  private CustomUserProvider() {
-  }
-
-  private static List<LCChatKitUser> partUsers = new ArrayList<>();
-
-  // 此数据均为 fake，仅供参考
-  static {
-    partUsers.add(new LCChatKitUser("5", "亦兮", "http://www.avatarsdb.com/avatars/tom_and_jerry2.jpg"));
-    partUsers.add(new LCChatKitUser("2", "独秀同学", "http://www.avatarsdb.com/avatars/jerry.jpg"));
-    partUsers.add(new LCChatKitUser("3", "Harry", "http://www.avatarsdb.com/avatars/young_harry.jpg"));
-    partUsers.add(new LCChatKitUser("4", "William", "http://www.avatarsdb.com/avatars/william_shakespeare.jpg"));
-    partUsers.add(new LCChatKitUser("Bob", "Bob", "http://www.avatarsdb.com/avatars/bath_bob.jpg"));
-  }
-
-  @Override
-  public void fetchProfiles(List<String> list, LCChatProfilesCallBack callBack) {
-    List<LCChatKitUser> userList = new ArrayList<>();
-    for (String userId : list) {
-      for (LCChatKitUser user : partUsers) {
-        if (user.getUserId().equals(userId)) {
-          userList.add(user);
-          break;
+    public synchronized static CustomUserProvider getInstance() {
+        if (null == customUserProvider) {
+            customUserProvider = new CustomUserProvider();
         }
-      }
+        return customUserProvider;
     }
-    callBack.done(userList, null);
-  }
 
-  @Override
-  public List<LCChatKitUser> getAllUsers() {
-    return partUsers;
-  }
+    private CustomUserProvider() {
+    }
+
+    private static List<LCChatKitUser> partUsers = new ArrayList<>();
+
+    // 此数据均为 fake，仅供参考
+    static {
+        partUsers.add(new LCChatKitUser("5", "亦兮", "http://www.avatarsdb.com/avatars/tom_and_jerry2.jpg"));
+        partUsers.add(new LCChatKitUser("2", "独秀同学", "http://www.avatarsdb.com/avatars/jerry.jpg"));
+        partUsers.add(new LCChatKitUser("3", "Harry", "http://www.avatarsdb.com/avatars/young_harry.jpg"));
+        partUsers.add(new LCChatKitUser("4", "William", "http://www.avatarsdb.com/avatars/william_shakespeare.jpg"));
+        partUsers.add(new LCChatKitUser("Bob", "Bob", "http://www.avatarsdb.com/avatars/bath_bob.jpg"));
+    }
+
+    @Override
+    public void fetchProfiles(List<String> list, LCChatProfilesCallBack callBack) {
+        LogUtil.d("fetchProfiles   list=" + new Gson().toJson(list));
+        List<LCChatKitUser> userList = new ArrayList<>();
+        for (String userId : list) {
+            for (LCChatKitUser user : partUsers) {
+                if (user.getUserId().equals(userId)) {
+                    userList.add(user);
+                    break;
+                }
+            }
+        }
+        callBack.done(userList, null);
+    }
+
+    @Override
+    public List<LCChatKitUser> getAllUsers() {
+        return partUsers;
+    }
 }
