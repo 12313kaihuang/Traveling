fdc1ed5868cf60beaea6600d466d70256193fbf9
huyu <2356048622@qq.com>
2025-03-26 16:46:30 +0800

    feat:添加SplashScreen库依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
 //    implementation(libs.androidx.activity.compose)
 //    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.splashscreen)
 //    implementation(libs.androidx.ui)
 //    implementation(libs.androidx.ui.graphics)
 //    implementation(libs.androidx.ui.tooling.preview)
